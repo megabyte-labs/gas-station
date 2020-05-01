f4dab132a8a384f9184026d7d0c2538ad9235887
Brian Zalewski <brian@enviedsolutions.com>
2020-05-01 23:55:03 +0000

    Update init-wsl.sh

--- windows/init-wsl.sh
+++ windows/init-wsl.sh
@@ -2,6 +2,7 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git ansible python3-pip libffi-dev libssl-dev -y
 pip3 install pywinrm[credssp]
+cd ~
 git clone https://gitlab.com/ProfessorManhattan/playbooks.git
 cd playbooks
 chmod 755 windows

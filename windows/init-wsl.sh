6a65032ed8d9bacbbadbd56b88dfe3a5a6434646
Brian Zalewski <bzalewski@manh.com>
2020-05-01 20:21:26 -0400

    Commit

--- windows/init-wsl.sh
+++ windows/init-wsl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+git_repo=https://gitlab.com/ProfessorManhattan/playbooks.git
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git ansible python3-pip libffi-dev libssl-dev -y
 pip3 install pywinrm[credssp]
-rm -rf $HOME/ProfessorManhattan && git clone https://gitlab.com/ProfessorManhattan/playbooks.git $HOME/ProfessorManhattan && chmod 755 $HOME/ProfessorManhattan/windows && export ANSIBLE_CONFIG=$HOME/ProfessorManhattan/windows && ansible-playbook $HOME/ProfessorManhattan/windows/main.yml
+# Needs to be on one line due to Powershell adding /r to end of everything
+rm -rf $HOME/ProfessorManhattan && git clone $git_repo $HOME/ProfessorManhattan && chmod 755 $HOME/ProfessorManhattan/windows && pushd $HOME/ProfessorManhattan/windows && ansible-playbook main.yml

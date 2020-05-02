7959f1c6c6c70ffcfd0807689ea49cc4734de753
Brian Zalewski <bzalewski@manh.com>
2020-05-01 20:08:18 -0400

    Commit

--- windows/init-wsl.sh
+++ windows/init-wsl.sh
@@ -2,4 +2,4 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git ansible python3-pip libffi-dev libssl-dev -y
 pip3 install pywinrm[credssp]
-git clone https://gitlab.com/ProfessorManhattan/playbooks.git $HOME/ProfessorManhattan && chmod 755 $HOME/ProfessorManhattan/windows && ansible-playbook $HOME/ProfessorManhattan/windows/main.yml
+git clone https://gitlab.com/ProfessorManhattan/playbooks.git $HOME/ProfessorManhattan && chmod 755 $HOME/ProfessorManhattan/windows && cd $HOME/ProfessorManhattan/windows && ansible-playbook main.yml

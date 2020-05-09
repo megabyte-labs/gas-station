3d13ed3618568858698a1ad877b22e6a7bf9e4e6
Brian Zalewski <bzalewski@manh.com>
2020-05-08 20:43:04 -0400

    Commit

--- files/windows/windows-controller-init.ps1
+++ files/windows/windows-controller-init.ps1
@@ -1,15 +1,23 @@
+Write-Host "Enabling the Microsoft Windows Subsystem Linux feature" -ForegroundColor DarkGreen -BackgroundColor White
 Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" -All
+Write-Host "Downloading the script used to enable Ansible management" -ForegroundColor DarkGreen -BackgroundColor White
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+Write-Host "Running the Ansible management script" -ForegroundColor DarkGreen -BackgroundColor White
 powershell.exe -ExecutionPolicy ByPass -File $file -Verbose -EnableCredSSP -DisableBasicAuth
+Write-Host "Downloading/setting up a script that will continue the installation after reboot" -ForegroundColor DarkGreen -BackgroundColor White
 $nextRunUrl = "https://gitlab.com/ProfessorManhattan/Playbooks/-/raw/master/files/windows/windows-ubuntu-setup.ps1"
 $nextRunFile = "C:\professor-manhattan-windows-ubuntu-setup.ps1"
 (New-Object -TypeName System.Net.WebClient).DownloadFile($nextRunUrl, $nextRunFile)
 $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:24
 Register-ScheduledJob -Trigger $trigger -FilePath $nextRunFile -Name ProfessorManhattanPlaybookUbuntuSetup
+Write-Host "WSL and Windows RE are now enabled. Now, you have to download Ubuntu from the app store by following the instructions below." -ForegroundColor DarkGreen -BackgroundColor White
+Write-Host "1. Open the Microsoft Windows Store app from the Start Menu" -ForegroundColor DarkGreen -BackgroundColor White
+Write-Host "2. Search for Ubuntu" -ForegroundColor DarkGreen -BackgroundColor White
+Write-Host "1. Open the Microsoft Windows Store app from the Start Menu" -ForegroundColor DarkGreen -BackgroundColor White
+Write-Host "1. Open the Microsoft Windows Store app from the Start Menu" -ForegroundColor DarkGreen -BackgroundColor White
 echo "WSL and Windows RE are now enabled. Now, you have to download Ubuntu from the app store."
-echo "1. Open the Microsoft Windows Store app"
-echo "2. Search for Ubuntu"
-echo "3. Install it"
-echo "4. Reboot and this script will automatically continue"
+echo "1. Navigate to the Ubuntu app in the Windows App Store (https://www.microsoft.com/en-us/p/ubuntu/9nblggh4msv6)"
+echo "2. Install it"
+echo "3. Reboot and this script will automatically continue"
--- files/windows/windows-ubuntu-setup.ps1
+++ files/windows/windows-ubuntu-setup.ps1
@@ -1,12 +1,14 @@
 Remove-Item -path "C:\professor-manhattan-windows-ubuntu-setup.ps1"
 cd $HOME/Documents
-echo "Running in Ubuntu WSL: sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y install git ansible python3-pip libffi-dev libssl-dev"
+Write-Host "Updating Ubuntu and installing dependencies." -ForegroundColor DarkGreen -BackgroundColor White
 ubuntu run sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y install git ansible python3-pip libffi-dev libssl-dev && sudo dpkg-reconfigure openssh-server
+Write-Host "Installing Python dependencies" -ForegroundColor DarkGreen -BackgroundColor White
 ubuntu run pip3 install pywinrm[credssp]
+Write-Host "Cloning the Playbooks to your Documents folder" -ForegroundColor DarkGreen -BackgroundColor White
 ubuntu run git clone https://gitlab.com/ProfessorManhattan/Playbooks.git
 # Can't figure out how to add "&& ansible-playbook main.yml" to the end of this - can anybody figure this out?
 # So these steps are added:
-echo "Now run the following commands:"
-echo "1. ubuntu --- this will open the Ubuntu shell"
-echo "2. cd /mnt/c/Users/USER_FOLDER_NAME/Documents/Playbooks"
-echo "3. ansible-playbook main.yml"
+Write-Host "Set up finished! Now, run the following commands to run the playbooks:" -ForegroundColor DarkGreen -BackgroundColor White
+Write-Host "1. ubuntu --- this will open the Ubuntu shell" -ForegroundColor DarkGreen -BackgroundColor White
+Write-Host "2. cd /mnt/c/Users/USER_FOLDER_NAME/Documents/Playbooks" -ForegroundColor DarkGreen -BackgroundColor White
+Write-Host "3. ansible-playbook main.yml" -ForegroundColor DarkGreen -BackgroundColor White

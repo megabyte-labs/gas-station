6c0737c70dda8acce7208de1ccd8d5e5fc9b87d6
Brian Zalewski <brian@megabyte.space>
2020-08-21 01:50:59 -0400

    Quick commit.

--- files/ubuntu/master-continue.sh
+++ files/ubuntu/master-continue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Playbooks
 rm ~/.config/autostart/ansible-installer.desktop
-ansible-playbook --ask-vault-pass -i inventories/workstation.yml main.yml
+ansible-playbook --vault-password-file=.vault_pass -i inventories/workstation.yml main.yml

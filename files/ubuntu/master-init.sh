23c7850ec640516b48e4aae35129d12fb51c3c92
Brian Zalewski <brian@enviedsolutions.com>
2020-09-02 21:11:11 +0000

    Update files/ubuntu/master-init.sh

--- files/ubuntu/master-init.sh
+++ files/ubuntu/master-init.sh
@@ -9,3 +9,4 @@ cp ~/Playbooks/files/ubuntu/ansible-installer.desktop ~/.config/autostart/ansibl
 chmod 700 ~/Playbooks/files/ubuntu/master-continue.sh
 ansible-galaxy install -r requirements.yml
 ansible-playbook --ask-vault-pass -i inventories/workstation.yml start.yml
+sudo reboot

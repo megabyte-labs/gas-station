baa3aadd7fac93d40d6595309ca2427143e5d6c0
Brian Zalewski <brian@enviedsolutions.com>
2020-11-22 06:37:42 +0000

    Update files/ubuntu/master-init.sh

--- files/ubuntu/master-init.sh
+++ files/ubuntu/master-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y git python3 python3-pip
-pip3 install ansible-base
+sudo python3 -m pip install ansible
 cd ~
 if [ ! -d ~/Playbooks ] ; then
     git clone https://gitlab.com/ProfessorManhattan/Playbooks.git

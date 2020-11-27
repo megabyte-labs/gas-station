3c6dc29e77ee0f00dd9ca4ec70a82e3e005732ca
AngelDeaD <angeldead.trivic67@gmail.com>
2020-11-27 17:39:55 +0100

    Script that executes command in every role directory

---
+++ misc/pass-command.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ "$@" == *'"'* ]; then
+	echo You have to use double quotes
+	echo 'Usage: ./pass-command.sh "molecule test" (True)'
+	exit 1;
+fi
+
+if [[ "$2" == 'True' || "$2" == 'Yes' ]]; then
+	set -e
+fi
+
+# Directory where gitlab repository is located (need to point to directory where roles directory is)
+git_repository_dir="/home/hawkwood/Playbooks"
+
+find "$git_repository_dir" -mindepth 3 -maxdepth 3 -type d -not -path '*/\.*' | while read -r gitrepo
+do
+	cd "$gitrepo" || continue
+	$1
+done

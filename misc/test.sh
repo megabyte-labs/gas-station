7c73d3e3993a176f9d3e454b91520293cf176c0a
Brian Zalewski <brian@megabyte.space>
2020-12-02 01:01:26 -0500

    Moving to new templated test system.

---
+++ misc/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git_repository_dir="/home/hawkwood/Playbooks"
+
+find "$git_repository_dir/roles" -mindepth 2 -maxdepth 2 -type d -not -path "*/\.*" | while read -r gitrepo
+do
+    cd "$gitrepo" || continue
+    rm -rf common
+    cp -rT /home/hawkwood/Playbooks/misc/common .
+    CURRENT=`pwd`
+    BASENAME=`basename "$CURRENT"`
+    sed -i "s/{{ role_name }}/$BASENAME/" ./molecule/default/playbook.yml
+done

449ff3199fb7ba1816f6dc51c4b78902a463d9ab
Brian Zalewski <bzalewski@manh.com>
2021-02-17 21:35:25 -0500

    Added base files.

--- misc/overlay.sh
+++ misc/overlay.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Directory where files for import are located (need to point to root directory of ansible role)
-import_files="/home/hawkwood/Playbooks/misc/common"
+import_files="/Users/bzalewski/Playbooks/misc/common"
 # Directory where gitlab repository is located (need to point to directory where roles directory is)
-git_repository_dir="/home/hawkwood/Playbooks"
+git_repository_dir="/Users/bzalewski/Playbooks"
 
 if [[ -z "$import_files" || -z "$git_repository_dir" ]]; then
 	echo "You have not filled variables!"
@@ -12,7 +12,8 @@ if [[ -d "$git_repository_dir/roles" ]]; then
 	# added "not path" part to avoid copying files into .git directory and other hidden directories
 	find "$git_repository_dir/roles" -mindepth 2 -maxdepth 2 -type d -not -path '*/\.*' | while read -r gitrepo
 	do
-		find "$import_files" -maxdepth 1 -mindepth 1 -printf "%f\n" | while read -r skriven
+    echo $import_files
+		gfind "$import_files" -maxdepth 1 -mindepth 1 -printf "%f\n" | while read -r skriven
                 do
                         cp -Rf "$import_files/$skriven" "$gitrepo"
                 done

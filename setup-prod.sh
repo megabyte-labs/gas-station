2a2b8fbb6dff046313d5184883836e5c50323366
Brian Zalewski <brian@megabyte.space>
2021-03-04 22:12:40 -0500

    Removing unneeded files.

--- setup-prod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf group_vars
-rm -rf host_vars
-rm -rf inventories
-ln -s ./environments/prod/group_vars group_vars
-ln -s ./environments/prod/host_vars host_vars
-ln -s ./environments/prod/inventories inventories

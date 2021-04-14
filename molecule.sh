8bf1591898cd6f32b16b01b05b1975cc816ed825
Brian Zalewski <brian@megabyte.space>
2021-04-14 13:50:27 -0400

    [skip ci] Commit

--- molecule.sh
+++ molecule.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Determines which roles have been modified and then loops
+# through them, running `molecule test -s docker`
+
 GIT_DIFF_FILES=$(git diff-tree --no-commit-id --name-only -r "$CI_COMMIT_SHA")
 MODIFIED_DIRS=()
 for i in "${GIT_DIFF_FILES[@]}"
@@ -6,16 +10,21 @@ do
     if [[ $i =~ \.yml$ ]]; then
         TRIMMED_DIR="$(echo "$i" | cut -f 1,2,3 -d '/')"
         if [[ ! " ${MODIFIED_DIRS[*]} " =~ ${TRIMMED_DIR} ]]; then
-            echo "YAAA"
-            echo "$i"
             MODIFIED_DIRS[${#MODIFIED_DIRS[@]}]=${TRIMMED_DIR}
         fi
     fi
 done
 
+EXIT_CODE_SUM=0
 for j in "${MODIFIED_DIRS[@]}"
 do
-    cd "$j" || exit
-    molecule test -s docker || continue
-    cd ../../.. || exit
+    cd "$j" || continue
+    set -e
+    molecule test -s docker
+    EXIT_CODE=$?
+    EXIT_CODE_SUM=$(($EXIT_CODE_SUM + $EXIT_CODE))
+    set +e
+    cd ../../.. || continue
 done
+
+exit "$EXIT_CODE_SUM"

b0a85a4c8f22537a587a286c7c82ca57dece19ee
Brian Zalewski <brian@megabyte.space>
2021-04-14 14:50:20 -0400

    Triggering CI.

--- molecule.sh
+++ molecule.sh
@@ -3,9 +3,11 @@
 # Determines which roles have been modified and then loops
 # through them, running `molecule test -s docker`
 
-GIT_DIFF_FILES=$(git diff-tree --no-commit-id --name-only -r "$CI_COMMIT_SHA")
+GIT_DIFF_FILES=$(git diff-tree --no-commit-id --name-only -r 14e85d62)
 MODIFIED_DIRS=()
-for i in "${GIT_DIFF_FILES[@]}"
+GIT_DIFF_FILES_ARRAY=()
+while IFS='' read -r line; do GIT_DIFF_FILES_ARRAY+=("$line"); done < <(echo "$GIT_DIFF_FILES" | tr ' ' '\n')
+for i in "${GIT_DIFF_FILES_ARRAY[@]}"
 do
     if [[ $i =~ \.yml$ ]]; then
         TRIMMED_DIR="$(echo "$i" | cut -f 1,2,3 -d '/')"
@@ -19,11 +21,8 @@ EXIT_CODE_SUM=0
 for j in "${MODIFIED_DIRS[@]}"
 do
     cd "$j" || continue
-    set -e
-    molecule test -s docker
-    EXIT_CODE=$?
-    EXIT_CODE_SUM=$($EXIT_CODE_SUM + $EXIT_CODE)
-    set +e
+    molecule test -s docker || EXIT_CODE=$?
+    EXIT_CODE_SUM=$((EXIT_CODE_SUM + EXIT_CODE))
     cd ../../.. || continue
 done
 

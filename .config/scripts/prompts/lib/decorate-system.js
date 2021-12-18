923e6b57826ceb7c24547bed162ccde9b0a9b436
Brian Zalewski <brian@megabyte.space>
2021-12-18 14:11:09 -0500

    Squashed 'roles/languages/rust/' content from commit 097b1826
    git-subtree-dir: roles/languages/rust
    git-subtree-split: 097b1826883f8f64b9d989f3a5c7a3535e85f385

---
+++ .config/scripts/prompts/lib/decorate-system.js
@@ -0,0 +1,29 @@
+import chalk from 'chalk'
+
+/**
+ * Styles prompt choices for operating systems by
+ * adding a styled dot next to the OS entry.
+ *
+ * @param {string} name - The operating system name, in any format
+ * @returns {string} The name with a styled dot prepended to the string
+ */
+export function decorateSystem(name) {
+  const lower = name.toLowerCase()
+  if (lower.includes('archlinux')) {
+    return `${chalk.cyan('●')} ${name}`
+  } else if (lower.includes('centos')) {
+    return `${chalk.magenta('●')} ${name}`
+  } else if (lower.includes('debian')) {
+    return `${chalk.red('●')} ${name}`
+  } else if (lower.includes('fedora')) {
+    return `${chalk.blue('●')} ${name}`
+  } else if (lower.includes('ubuntu')) {
+    return `${chalk.yellow('●')} ${name}`
+  } else if (lower.includes('mac')) {
+    return `${chalk.white('●')} ${name}`
+  } else if (lower.includes('windows')) {
+    return `${chalk.green('●')} ${name}`
+  }
+
+  return `${chalk.cyan('●')} ${name}`
+}

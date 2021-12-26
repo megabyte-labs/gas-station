e618a635cf5872ef41561791ba8ba300d01e55cf
Megabyte Labs Automation <help@megabyte.space>
2021-12-26 10:57:40 +0000

    ⤵️ automation(synchronize) Applying changes from upstream repository.

--- .config/scripts/prompts/molecule/ssh.js
+++ .config/scripts/prompts/molecule/ssh.js
@@ -21,7 +21,7 @@ async function promptForSSHDetails() {
       type: 'input'
     },
     {
-      default: 'ansible',
+      default: 'root',
       message: 'What is the username of a user that has both sudo and SSH privileges?',
       name: 'user',
       type: 'input'
@@ -61,8 +61,8 @@ async function run() {
   // eslint-disable-next-line functional/no-try-statement
   try {
     return execSync(
-      `TEST_HOST=${details.host} TEST_PASSWORD=${details.password} TEST_BECOME_PASSWORD=${details.password}
-      TEST_PORT=${details.port} TEST_SSH_USER=${details.user} TEST_USER=${details.user}
+      `TEST_HOST=${details.host} TEST_PASSWORD=${details.password} TEST_BECOME_PASSWORD=${details.password} \
+      TEST_PORT=${details.port} TEST_SSH_USER=${details.user} TEST_USER=${details.user} poetry run \
       task ansible:test:molecule:ssh:cli`,
       { stdio: 'inherit' }
     )

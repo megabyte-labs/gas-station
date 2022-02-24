42836b928f48c5cc35f1d27a596661860fe71831
Brian Zalewski <brian@megabyte.space>
2022-02-23 19:02:00 -0500

    ☁️ Automation

--- roles/languages/deno/.config/scripts/start.sh
+++ roles/languages/deno/.config/scripts/start.sh
@@ -258,7 +258,12 @@ function installTask() {
     mkdir -p "$TARGET_BIN_DIR"
   fi
   mv "$TMP_DIR/task/task" "$TARGET_DEST"
-  logger success "Installed Task to $TARGET_DEST"
+  if type sudo &> /dev/null && sudo -n true; then
+    sudo mv "$TARGET_DEST" /usr/local/bin/task
+    logger success "Installed Task to /usr/local/bin/task"
+  else
+    logger success "Installed Task to $TARGET_DEST"
+  fi
   rm "$CHECKSUM_DESTINATION"
   rm "$DOWNLOAD_DESTINATION"
 }
@@ -343,7 +348,7 @@ fi
 if [[ "$OSTYPE" == 'darwin'* ]] || [[ "$OSTYPE" == 'linux-gnu'* ]] || [[ "$OSTYPE" == 'linux-musl'* ]]; then
   if [ -z "$INIT_CWD" ]; then
     if ! type brew &> /dev/null; then
-      if sudo -n true; then
+      if type sudo &> /dev/null && sudo -n true; then
         echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       else
         logger warn "Homebrew is not installed. The script will attempt to install Homebrew and you might be prompted for your password."
@@ -390,10 +395,12 @@ fi
 ensureTaskInstalled
 
 # @description Run the start logic, if appropriate
-if [ -z "$GITLAB_CI" ] && [ -z "$INIT_CWD" ] && [ -f Taskfile.yml ]; then
+if [ -z "$CI" ] && [ -z "$INIT_CWD" ] && [ -f Taskfile.yml ]; then
   # shellcheck disable=SC1091
   . "$HOME/.profile"
-  task start
-  # shellcheck disable=SC2016
-  logger info 'There may have been changes to your PATH variable. You may have to reload your terminal or run:\n\n`. "$HOME/.profile"`'
+  if task donothing &> /dev/null; then
+    task start
+    # shellcheck disable=SC2016
+    logger info 'There may have been changes to your PATH variable. You may have to reload your terminal or run:\n\n`. "$HOME/.profile"`'
+  fi
 fi

37273cca3252c47842c5c1eb9468ef9ac804cd7d
Megabyte Labs <help@megabyte.space>
2022-05-20 06:37:31 -0400

    ☁️ chore(automation): Applying changes from upstream repository.

--- start.sh
+++ start.sh
@@ -450,6 +450,10 @@ function ensureTaskfiles() {
         rm -rf shared-master
       fi
     fi
+    if [ -n "$BOOTSTRAP_EXIT_CODE" ] && ! task donothing; then
+      # task donothing still does not work so issue must be with main Taskfile.yml
+      git checkout HEAD~1 -- Taskfile.yml
+    fi
   fi
 }
 
@@ -570,7 +574,12 @@ if [ -d .git ] && type git &> /dev/null; then
       git reset --hard origin/master
       git push --force origin synchronize || FORCE_SYNC_ERR=$?
       if [ -n "$FORCE_SYNC_ERR" ] && type task &> /dev/null; then
-        NO_GITLAB_SYNCHRONIZE=true task ci:synchronize
+        NO_GITLAB_SYNCHRONIZE=true task ci:synchronize || CI_SYNC_TASK_ISSUE=$?
+        if [ -n "$CI_SYNC_TASK_ISSUE" ]; then
+          logger warn 'Possible issue with `Taskfile.yml` -- attempting to fix by reverting `Taskfile.yml` to previous commit'
+          git checkout HEAD~1 -- Taskfile.yml
+          NO_GITLAB_SYNCHRONIZE=true task ci:synchronize
+        fi
       else
         DELAYED_CI_SYNC=true
       fi

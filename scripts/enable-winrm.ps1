e5e63171ac928ff05f8f66661a3e77a36c9e1ccf
Brian Zalewski <brian@megabyte.space>
2022-07-09 23:21:13 +0000

    Update scripts/quickstart.ps1, scripts/quickstart.sh, scripts/enable-winrm.ps1

--- scripts/enable-winrm.ps1
+++ scripts/enable-winrm.ps1
@@ -1,5 +1,12 @@
-# This file should be run on any Windows hosts you are trying to provision with Ansible. It
-# enables WinRE which is required for managing Windows hosts.
+#Requires -RunAsAdministrator
+
+# @file scripts/enable-winrm.ps1
+# @brief Enables WinRM on a Windows machine
+# @description
+#   Runs the ConfigureRemotingForAnsible.ps1 script with the EnableCredSSP option. It also
+#   ensures BasicAuth is disabled. After running this script, you should be able to connect to the
+#   Windows instance from a WSL1 environment. WSL2 environments do not have access to the LAN
+#   IP address so a WSL1 environment is preferred.
 
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"

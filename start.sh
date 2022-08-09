44f9020a5d31d2672bdd24a1489279a2dcbbc6bd
Megabyte Labs <help@megabyte.space>
2022-08-08 22:50:13 -0400

    ☁️ chore(automation): Applying changes from upstream repository.

--- start.sh
+++ start.sh
@@ -102,6 +102,54 @@ function logger() {
   fi
 }
 
+# @description Helper function for ensurePackageInstalled for Alpine installations
+function ensureAlpinePackageInstalled() {
+  if type sudo &> /dev/null && [ "$CAN_USE_SUDO" != 'false' ]; then
+    sudo apk --no-cache add "$1"
+  else
+    apk --no-cache add "$1"
+  fi
+}
+
+# @description Helper function for ensurePackageInstalled for ArchLinux installations
+function ensureArchPackageInstalled() {
+  if type sudo &> /dev/null && [ "$CAN_USE_SUDO" != 'false' ]; then
+    sudo pacman update
+    sudo pacman -S "$1"
+  else
+    pacman update
+    pacman -S "$1"
+  fi
+}
+
+# @description Helper function for ensurePackageInstalled for Debian installations
+function ensureDebianPackageInstalled() {
+  if type sudo &> /dev/null && [ "$CAN_USE_SUDO" != 'false' ]; then
+    sudo apt-get update
+    sudo apt-get install -y "$1"
+  else
+    apt-get update
+    apt-get install -y "$1"
+  fi
+}
+
+# @description Helper function for ensurePackageInstalled for RedHat installations
+function ensureRedHatPackageInstalled() {
+  if type sudo &> /dev/null && [ "$CAN_USE_SUDO" != 'false' ]; then
+    if type dnf &> /dev/null; then
+      sudo dnf install -y "$1"
+    else
+      sudo yum install -y "$1"
+    fi
+  else
+    if type dnf &> /dev/null; then
+      dnf install -y "$1"
+    else
+      yum install -y "$1"
+    fi
+  fi
+}
+
 # @description Installs package when user is root on Linux
 #
 # @example
@@ -112,22 +160,17 @@ function logger() {
 # @exitcode 0 The package was successfully installed
 # @exitcode 1+ If there was an error, the package needs to be installed manually, or if the OS is unsupported
 function ensureRootPackageInstalled() {
+  export CAN_USE_SUDO='false'
   if ! type "$1" &> /dev/null; then
     if [[ "$OSTYPE" == 'linux'* ]]; then
       if [ -f "/etc/redhat-release" ]; then
-        if type dnf &> /dev/null; then
-          dnf install -y "$1"
-        else
-          yum install -y "$1"
-        fi
-      elif [ -f "/etc/lsb-release" ]; then
-        apt-get update
-        apt-get install -y "$1"
+        ensureRedHatPackageInstalled "$1"
+      elif [ -f "/etc/debian_version" ]; then
+        ensureDebianPackageInstalled "$1"
       elif [ -f "/etc/arch-release" ]; then
-        pacman update
-        pacman -S "$1"
+        ensureArchPackageInstalled "$1"
       elif [ -f "/etc/alpine-release" ]; then
-        apk --no-cache add "$1"
+        ensureAlpinePackageInstalled "$1"
       fi
     fi
   fi
@@ -191,46 +234,27 @@ function ensureLocalPath() {
 # @exitcode 0 The package(s) were successfully installed
 # @exitcode 1+ If there was an error, the package needs to be installed manually, or if the OS is unsupported
 function ensurePackageInstalled() {
+  export CAN_USE_SUDO='true'
   if ! type "$1" &> /dev/null; then
     if [[ "$OSTYPE" == 'darwin'* ]]; then
       brew install "$1"
     elif [[ "$OSTYPE" == 'linux'* ]]; then
-      if [ -f "/etc/redhat-release" ] || type dnf &> /dev/null || type yum &> /dev/null; then
-        if type sudo &> /dev/null; then
-          if type dnf &> /dev/null; then
-            sudo dnf install -y "$1"
-          else
-            sudo yum install -y "$1"
-          fi
-        else
-          if type dnf &> /dev/null; then
-            dnf install -y "$1"
-          else
-            yum install -y "$1"
-          fi
-        fi
-      elif [ -f "/etc/lsb-release" ] || type apt-get &> /dev/null; then
-        if type sudo &> /dev/null; then
-          sudo apt-get update
-          sudo apt-get install -y "$1"
-        else
-          apt-get update
-          apt-get install -y "$1"
-        fi
-      elif [ -f "/etc/arch-release" ] || type pacman &> /dev/null; then
-        if type sudo &> /dev/null; then
-          sudo pacman update
-          sudo pacman -S "$1"
-        else
-          pacman update
-          pacman -S "$1"
-        fi
-      elif [ -f "/etc/alpine-release" ] || type apk &> /dev/null; then
-        if type sudo &> /dev/null; then
-          sudo apk --no-cache add "$1"
-        else
-          apk --no-cache add "$1"
-        fi
+      if [ -f "/etc/redhat-release" ]; then
+        ensureRedHatPackageInstalled "$1"
+      elif [ -f "/etc/debian_version" ]; then
+        ensureDebianPackageInstalled "$1"
+      elif [ -f "/etc/arch-release" ]; then
+        ensureArchPackageInstalled "$1"
+      elif [ -f "/etc/alpine-release" ]; then
+        ensureAlpinePackageInstalled "$1"
+      elif type dnf &> /dev/null || type yum &> /dev/null; then
+        ensureRedHatPackageInstalled "$1"
+      elif type apt-get &> /dev/null; then
+        ensureDebianPackageInstalled "$1"
+      elif type pacman &> /dev/null; then
+        ensureArchPackageInstalled "$1"
+      elif type apk &> /dev/null; then
+        ensureAlpinePackageInstalled "$1"
       else
         logger error "$1 is missing. Please install $1 to continue." && exit 1
       fi

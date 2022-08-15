fadf3cf7835d28f581a3fef9416cd0286fd6c7b6
Brian Zalewski <brian@megabyte.space>
2022-08-15 01:35:05 +0000

    Update scripts/qubes.sh, ansible.cfg

--- scripts/qubes.sh
+++ scripts/qubes.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -d "$HOME/Playbooks" ]; then
-  echo "Remove the $HOME/Playbooks directory before continuing" && exit 1
-fi
-
 # Download Gas Station and transfer to dom0 via DispVM
 qvm-create ansible-playbooks-dvm --class DispVM --label=red --property=template=debian-11-minimal-dvm
 qvm-run ansible-dvm curl -sSL https://gitlab.com/megabyte-labs/gas-station/-/archive/master/gas-station-master.tar.gz -o Playbooks.tar.gz
@@ -12,8 +8,9 @@ tar -xzvf '/tmp/Playbooks.tar.gz' &> /dev/null
 rm '/tmp/Playbooks.tar.gz'
 
 # Move files to appropriate locations
+sudo rm -rf '/etc/ansible'
 sudo mv gas-station-master '/etc/ansible'
 sudo mkdir -p '/usr/share/ansible/plugins/connection'
 sudo ln -s '/etc/ansible/scripts/connection/qubes.py' '/usr/share/ansible/plugins/connection'
-sudo mkdir -p '/usr/share/ansible_qubes'
-sudo ln -s '/etc/ansible/scripts/library/qubesos.py' '/usr/share/ansible_qubes/qubesos.py'
+sudo mkdir -p '/usr/share/ansible/library'
+sudo ln -s '/etc/ansible/scripts/library/qubesos.py' '/usr/share/ansible/library/qubesos.py'

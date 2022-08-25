1fd0d4ce85aa102a8cb6b16ce02aa7974c701764
Megabyte Labs <help@megabyte.space>
2022-08-24 22:53:02 -0400

    ☁️ chore(automation): Applying changes from upstream repository.

--- scripts/quickstart.ps1
+++ scripts/quickstart.ps1
@@ -1,23 +1,26 @@
-#Requires -RunAsAdministrator
-
 # @file scripts/quickstart.ps1
-# @brief This script will help you easily take care of the requirements and then run [Gas Station](https://github.com/megabyte-labs/gas-station)
-#   on your Windows computer.
+# @brief This script allows you to perform a complete install of [Gas Station](https://github.com/megabyte-labs/gas-station)
+#   on your Windows computer with a single line of code. It is intended to be run on a freshly installed copy of Windows 11.
+#   A fresh install is really the only way we can guarantee a successful install.
 # @description
-#   1. This script will enable Windows features required for WSL.
-#   2. It will reboot and continue where it left off.
+#   1. This script will enable Windows features required for WSL / Docker Desktop.
+#   2. It will reboot and continue where it left off whenever a reboot is needed. It will reset the user's password to MegabyteLabs
+#      if the account is a local one (i.e. not a Microsoft online account) so that there is minimal required interaction.
 #   3. Ensures Windows WinRM is active and configured.
-#   4. Installs and pre-configures the WSL environment.
-#   5. Ensures Docker Desktop is installed
-#   6. Reboots and continues where it left off.
-#   7. The playbook is run.
+#   4. Performs set up of the WSL environment (if the WSL method is active - currently, only Docker has been thoroughly tested)
+#   5. Ensures Docker Desktop is installed and that the daemon is running.
+#   6. The LAN IP is detected and that same IP that the Windows computer has is used by Docker to provision with Ansible.
 
+# @description Configuration variables
+$AdminUsername = 'Gas'
+$AdminPassword = 'CrownNebula'
 # Uncomment this to provision with WSL instead of Docker
 # $ProvisionWithWSL = 'True'
 $QuickstartScript = "C:\Temp\quickstart.ps1"
 $QuickstartShellScript = "C:\Temp\quickstart.sh"
-# Change this to modify the password that the user account resets to
-$UserPassword = 'MegabyteLabs'
+
+# @description Shared common variables
+$LocalUser = (whoami).Substring((whoami).LastIndexOf('\') + 1)
 
 # @description Used to log styled messages
 function Log($message) {
@@ -42,20 +45,6 @@ function CheckForAdminRights() {
   }
 }
 
-# @description Checks for admin privileges and if there are none then open a new instance with Administrator rights
-$AdminRights = CheckForAdminRights
-$AdminRights
-if($AdminRights){
-  Log "Current session is an Administrator session.. Good."
-} else {
-  Log "This script requires Administrator privileges. Press ENTER to escalate to Administrator privileges."
-  Read-Host
-  [Environment]::Exit(0)
-}
-
-New-Item -ItemType Directory -Force -Path C:\Temp
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
-
 # @description Prepares the machine to automatically continue installation after a reboot
 function PrepareForReboot {
   if (!(Test-Path $QuickstartScript)) {
@@ -63,21 +52,25 @@ function PrepareForReboot {
     Start-BitsTransfer -Source "https://install.doctor/windows-quickstart" -Destination $QuickstartScript -Description "Downloading initialization script"
   }
   Log "Ensuring start-up script is present"
-  Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Gas Station.bat" "PowerShell.exe -ExecutionPolicy RemoteSigned -Command `"Start-Process -FilePath powershell -ArgumentList '-File C:\Temp\quickstart.ps1 -Verbose' -verb runas`""
-  $LocalUser = (whoami).Substring((whoami).LastIndexOf('\') + 1)
-  $AccountType = Get-LocalUser -Name $LocalUser | Select-Object -ExpandProperty PrincipalSource
-  if ($AccountType -eq 'Local') {
-    Log "Changing $env:Username password to '$UserPassword' so we can automatically log back in"
-    $NewPassword = ConvertTo-SecureString "$UserPassword" -AsPlainText -Force
-    Set-LocalUser -Name $env:Username -Password $NewPassword
-    Log "Turning on auto-logon"
-    $RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
-    Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "1" -Type String
-    Set-ItemProperty $RegistryPath 'DefaultUsername' -Value "$env:Username" -type String
-    Set-ItemProperty $RegistryPath 'DefaultPassword' -Value "MegabyteLabs" -type String
-  } else {
-    Log "Local user's account is a $AccountType account so auto-logging in after reboot is not supported"
-  }
+  Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Gas Station.bat" "PowerShell.exe -ExecutionPolicy RemoteSigned -Command `"Start-Process -FilePath powershell -ArgumentList '-File $QuickstartScript -Verbose' -verb runas`""
+  # Logic below from method where local type accounts had their passwords changed
+  # New method involves creating temporary local admin account
+  # $AccountType = Get-LocalUser -Name $LocalUser | Select-Object -ExpandProperty PrincipalSource
+  # if ($AccountType -eq 'Local') { }
+  Log "Creating temporary local administrator named $AdminUsername"
+  net user $AdminUsername /add
+  net localgroup administrators $AdminUsername /add
+  Log "Setting $AdminUsername password to $AdminPassword"
+  $SecureAdminPassword = ConvertTo-SecureString "$AdminPassword" -AsPlainText -Force
+  Set-LocalUser -Name $AdminUsername -Password $SecureAdminPassword
+  # For domain user password, use the following:
+  # Set-ADAccountPassword Tom -NewPassword $NewPassword -Reset
+  Log "Turning on auto-logon"
+  $RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+  Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "1" -Type String
+  Set-ItemProperty $RegistryPath 'DefaultUsername' -Value "$AdminUsername" -type String
+  # Following line needs to use unencrypted password
+  Set-ItemProperty $RegistryPath 'DefaultPassword' -Value "$AdminPassword" -type String
 }
 
 # @description Reboot and continue script after reboot
@@ -106,7 +99,7 @@ function EnsureWindowsUpdated {
   }
   Log "Ensuring all the available Windows updates have been applied."
   Import-Module PSWindowsUpdate -Force
-  Get-WUInstall -AcceptAll -IgnoreReboot
+  Get-WUInstall -AcceptAll -IgnoreReboot | Out-Null
   Log "Checking if reboot is required."
   RebootAndContinueIfRequired
 }
@@ -174,7 +167,7 @@ function EnsureDockerFunctional {
   Log "Ensuring WSL version is set to 2 (required for Docker Desktop)"
   wsl --set-default-version 2
   Log "Running test command (i.e. docker run --rm hello-world)"
-  docker run --rm hello-world
+  docker run --rm hello-world | Out-Null
   if ($?) {
     Log "Docker Desktop is operational! Continuing.."
   } else {
@@ -185,7 +178,7 @@ function EnsureDockerFunctional {
     & 'C:\Program Files\Docker\Docker\Docker Desktop.exe'
     Log "Waiting for Docker Desktop to come online"
     Start-Sleep -s 30
-    docker run --rm hello-world
+    docker run --rm hello-world | Out-Null
     if ($?) {
       Log "Docker is now running and operational! Continuing.."
     } else {
@@ -210,36 +203,42 @@ function EnableWinRM {
   $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
   (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
   powershell.exe -ExecutionPolicy ByPass -File $file -Verbose -EnableCredSSP -DisableBasicAuth -ForceNewSSLCert -SkipNetworkProfileCheck
-  # Generate OpenSSL configuration and encryption keys
-  Log "Ensuring OpenSSL is installed"
-  choco install -y -r openssl
-  $UsernameLowercase = $env:Username.ToLower()
-  $OpenSSLConfig = "C:\Temp\openssl.conf"
-  Set-Content -Path $OpenSSLConfig -Value @"
-distinguished_name = req_distinguished_name
-[req_distinguished_name]
-[v3_req_client]
-extendedKeyUsage = clientAuth
-subjectAltName = otherName:1.3.6.1.4.1.311.20.2.3;UTF8:$UsernameLowercase@localhost
-"@
-  $UserPEMPath = Join-Path "C:\Temp" user.pem
-  $KeyPEMPath = Join-Path "C:\Temp" key.pem
-  Log "Generating PEM files with OpenSSL"
-  & "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -x509 -nodes -days 365 -newkey rsa:2048 -out $UserPEMPath -outform PEM -keyout $KeyPEMPath -subj "/CN=$UsernameLowercase" -extensions v3_req_client 2>&1
-  #Remove-Item $OpenSSLConfig -Force
-  # Configure WinRM to use the generated configurations / credentials
-  Log "Importing PEM certificates"
-  Import-Certificate -FilePath $UserPEMPath -CertStoreLocation cert:\LocalMachine\root
-  $WinRMCert = Import-Certificate -FilePath $UserPEMPath -CertStoreLocation cert:\LocalMachine\TrustedPeople
-  $PasswordCred = ConvertTo-SecureString -AsPlainText -Force $UserPassword
-  $WinRMCreds = New-Object System.Management.Automation.PSCredential($UsernameLowercase, $PasswordCred) -ea Stop
-  Log "Configuring WinRM to use the certificates"
-  New-Item -Path WSMan:\localhost\ClientCertificate -Subject "$UsernameLowercase@localhost" -URI * -Issuer $WinRMCert.Thumbprint -Credential $WinRMCreds -Force
-  Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
-  # Restart WinRM
-  Log "Restarting WinRM"
-  Restart-Service -Name WinRM -Force
 }
+
+# @description More advanced procedure to establish a more secure connection (requires copying key files to Ansible host)
+# Source: https://vnuggets.com/2019/08/08/ansible-certificate-authentication-to-windows/
+# function EnableWinRMCertificate {
+#   # Generate OpenSSL configuration and encryption keys
+#   Log "Ensuring OpenSSL is installed"
+#   choco install -y -r openssl
+#   $UsernameLowercase = $env:Username.ToLower()
+#   $OpenSSLConfig = "C:\Temp\openssl.conf"
+#   # Note: This might need white space trimmed from beginning of each line since it's formatted as such
+#   Set-Content -Path $OpenSSLConfig -Value @"
+#     distinguished_name = req_distinguished_name
+#     [req_distinguished_name]
+#     [v3_req_client]
+#     extendedKeyUsage = clientAuth
+#     subjectAltName = otherName:1.3.6.1.4.1.311.20.2.3;UTF8:$UsernameLowercase@localhost
+#   "@
+#   $UserPEMPath = Join-Path "C:\Temp" user.pem
+#   $KeyPEMPath = Join-Path "C:\Temp" key.pem
+#   Log "Generating PEM files with OpenSSL"
+#   & "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -x509 -nodes -days 365 -newkey rsa:2048 -out $UserPEMPath -outform PEM -keyout $KeyPEMPath -subj "/CN=$UsernameLowercase" -extensions v3_req_client 2>&1
+#   #Remove-Item $OpenSSLConfig -Force
+#   # Configure WinRM to use the generated configurations / credentials
+#   Log "Importing PEM certificates"
+#   Import-Certificate -FilePath $UserPEMPath -CertStoreLocation cert:\LocalMachine\root
+#   $WinRMCert = Import-Certificate -FilePath $UserPEMPath -CertStoreLocation cert:\LocalMachine\TrustedPeople
+#   $PasswordCred = ConvertTo-SecureString -AsPlainText -Force $UserPassword
+#   $WinRMCreds = New-Object System.Management.Automation.PSCredential($UsernameLowercase, $PasswordCred) -ea Stop
+#   Log "Configuring WinRM to use the certificates"
+#   New-Item -Path WSMan:\localhost\ClientCertificate -Subject "$UsernameLowercase@localhost" -URI * -Issuer $WinRMCert.Thumbprint -Credential $WinRMCreds -Force
+#   Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
+#   # Restart WinRM
+#   Log "Restarting WinRM"
+#   Restart-Service -Name WinRM -Force
+# }
 
 # @description Run the playbook with Docker
 function RunPlaybookDocker {
@@ -259,7 +258,7 @@ function RunPlaybookDocker {
   }
   PrepareForReboot
   Log "Provisioning environment with Docker using $HostIP as the IP address"
-  docker run -it -v $("$($CurrentLocation)"+':/'+$WorkDirectory) -w $('/'+$WorkDirectory) --add-host='windows:'$HostIP --entrypoint /bin/bash megabytelabs/updater:latest-full ./quickstart.sh
+  docker run -it -v $("$($CurrentLocation)"+':/'+$WorkDirectory) -w $('/'+$WorkDirectory) -e MOLECULE_GROUP="windows" -e ANSIBLE_PASSWORD="$AdminPassword" -e ANSIBLE_USER="$AdminUsername" --add-host='standard:'$HostIP --entrypoint /bin/bash megabytelabs/updater:latest-full ./quickstart.sh
 }
 
 # @description Run the playbook with WSL
@@ -297,10 +296,25 @@ function ProvisionWindowsAnsible {
   } else {
     RunPlaybookDocker
   }
-  Log "All done! Make sure you change your password. It was set to 'MegabyteLabs' for automation purposes."
-  Read-Host "Press ENTER to exit, remove temporary files, and the start-up script"
-  Remove-Item -path "C:\Temp" -Recurse -Force
-  Remove-Item -path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Gas Station.bat" -Force
+  Read-Host "Removing temporary files (assuming you are not currently in the C:\Temp directory."
+  Remove-Item -path "C:\Temp" -Recurse -Force | Out-Null
+  Remove-Item -path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Gas Station.bat" -Force | Out-Null
+  Log "Removing temporary local administrator account named Byte"
+  Remove-LocalUser -Name "$AdminUsername"
 }
 
-ProvisionWindowsAnsible
+# @description Checks for admin privileges and if there are none then open a new instance with Administrator rights
+$AdminAccess = CheckForAdminRights
+if($AdminAccess){
+  Log "Current session is an Administrator session.. Good."
+  ProvisionWindowsAnsible
+} else {
+  if (!(Test-Path $QuickstartScript)) {
+    Log "Ensuring the recursive update script is downloaded"
+    Start-BitsTransfer -Source "https://install.doctor/windows-quickstart" -Destination $QuickstartScript -Description "Downloading initialization script"
+  }
+  Log "This script requires Administrator privileges. Press ENTER to escalate to Administrator privileges."
+  Read-Host
+  Start-Process PowerShell -verb runas -ArgumentList "-file $QuickstartScript"
+  Log "NOTE: In case there was an error or if you cancelled half-way through, make sure the administrator named Byte is removed."
+}

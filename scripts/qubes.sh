e8116293d4bd99f786c601f89efc39bda3ca443e
Brian Zalewski <brian@megabyte.space>
2022-09-19 06:55:02 +0000

    Update scripts/qubes.sh
    Former-commit-id: 75ba00428a83df705892ea0bc6288314f7516217

--- scripts/qubes.sh
+++ scripts/qubes.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# The VM name that will manage the Ansible provisioning
 ANSIBLE_PROVISION_VM="ansible-provision"
+# Whether or not to use dom0 to run the Ansible play (WIP)
 USE_DOM0="false"
 
 set -ex

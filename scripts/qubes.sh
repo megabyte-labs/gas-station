ca490eb0d3f6753a4d1bd9411f5e2bb048380b55
Brian Zalewski <brian@megabyte.space>
2022-11-03 03:25:19 +0000

    Update scripts/qubes.sh

--- scripts/qubes.sh
+++ scripts/qubes.sh
@@ -247,4 +247,4 @@ cd /etc/ansible
 if [[ "$(hostname)" == 'dom0' ]]; then
   export ANSIBLE_STDOUT_CALLBACK="default"
 fi
-ansible-playbook -vvvvv --skip-tags dotfiles --vault-password-file ~/.vaultpass -i /etc/ansible/inventories/quickstart.yml /etc/ansible/qubes.yml | tee ~/ansible-log.txt
+ansible-playbook --skip-tags dotfiles --vault-password-file ~/.vaultpass -i /etc/ansible/inventories/quickstart.yml /etc/ansible/qubes.yml | tee ~/ansible-log.txt
